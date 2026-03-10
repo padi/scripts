12fa865cc5987fc7a5fc55b9bb6a21636e715bd2
Marc Ignacio <padi@users.noreply.github.com>
2026-03-10 13:26:39 +0800

    fb story scraper now accepts a html file parameter

--- fbstory-get-videojson.sh
+++ fbstory-get-videojson.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-cat fbraw.html | grep ScheduledServerJS | grep VideoPlayerShakaPerformanceLoggerConfig >targetscripttag.html
+# FB Story Scraper
+# 1. In Chrome: Right click > View Source,
+# 2. Save the html file
+# 3. Use is it as a parameter here
+# 4. Result: returns relevant URLs for extraction
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <html_file>"
+  exit 1
+fi
+
+input_file="$1"
+
+if [ ! -f "$input_file" ]; then
+  echo "Error: File '$input_file' not found."
+  exit 1
+fi
+
+cat "$input_file" | grep ScheduledServerJS | grep VideoPlayerShakaPerformanceLoggerConfig >targetscripttag.html
 sed -n 's/.*<script[^>]*>\(.*\)<\/script>.*/\1/p' targetscripttag.html >target.json
 ./fbjson_url_extractor.rb target.json >testresult.txt
